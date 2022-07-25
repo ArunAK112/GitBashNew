ba08d2fe94c026497d0496c04ea13e466370ec0f
arun <arunofficial112@gmail.com>
2022-07-25 12:06:17 +0530

    new commit

---
+++ src/test/java/demoPackage/Test.java
@@ -0,0 +1,12 @@
+package demoPackage;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Checking");
+		System.out.println("Again checking");
+
+	}
+
+}
